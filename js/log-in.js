916be71e089c46c685c8eeebd042713c642c5c49
jiangyan-ava <jiangyan-ava@qq.com>
2017-11-10 21:20:09 +0800

    添加下划线效果

--- js/log-in.js
+++ js/log-in.js
@@ -1,12 +1,16 @@
 $('#sign-in-title-right').click(function () {
 	$(this).addClass('clickClass');
 	$('#sign-in-title-left').removeClass('clickClass');
-	$('#underline-right').style.display="visible";
+	$('#underline-left').hide();
+	$('#underline-right').show();
+	$("#log-a").attr("href","./manager-front.html");
 })
 $('#sign-in-title-left').click(function () {
 	$(this).addClass('clickClass');
 	$('#sign-in-title-right').removeClass('clickClass');
-	$('#underline-left').style.display="visible";
+	$('#underline-right').hide();
+	$('#underline-left').show();
+	$("#log-a").attr("href","./user-front.html"); 
 })
 $(function() {
 	$('#sign-in-title-left').trigger('click')
--- js/sign-in.js
+++ js/sign-in.js
@@ -1,12 +1,16 @@
 $('#sign-in-title-right').click(function () {
 	$(this).addClass('clickClass');
 	$('#sign-in-title-left').removeClass('clickClass');
-	$('#underline-right').style.display="visible";
+	$('#underline-left').hide();
+	$('#underline-right').show();
+	$("#sign-a").attr("href","./manager-front.html");//以不同身份登录，登录的链接连到不同的首页 
 })
 $('#sign-in-title-left').click(function () {
 	$(this).addClass('clickClass');
 	$('#sign-in-title-right').removeClass('clickClass');
-	$('#underline-left').style.display="visible";
+	$('#underline-right').hide();
+	$('#underline-left').show();
+	$("#sign-a").attr("href","./user-front.html"); 
 })
 $(function() {
 	$('#sign-in-title-left').trigger('click')

a0a6372c84d57dbd77efa0c7d08d0d9bdc818a43
jiangyan-ava <jiangyan-ava@qq.com>
2017-11-13 13:04:58 +0800

    剩余js周一晚之前写完

--- js/user-infopage.js
+++ js/user-infopage.js
@@ -1,4 +1,28 @@
-$('.message-title').find('span').click(
-	function () {
-		alert(1);
-	})
+$('#person-answer').click(function(){
+	$(this).addClass('clickClass');
+	 $(this).siblings().removeClass('clickClass');  
+})
+$('#person-question').click(function(){
+	$(this).addClass('clickClass');
+	 $(this).siblings().removeClass('clickClass');  
+})
+$('#person-guanzhu-user').click(function(){
+	$(this).addClass('clickClass');
+	 $(this).siblings().removeClass('clickClass');  
+})
+$('#person-fan').click(function(){
+	$(this).addClass('clickClass');
+	 $(this).siblings().removeClass('clickClass');  
+})
+$('#person-guanzhu-question').click(function(){
+	$(this).addClass('clickClass');
+	 $(this).siblings().removeClass('clickClass');  
+})
+$('#person-message').click(function(){
+	$(this).addClass('clickClass');
+	 $(this).siblings().removeClass('clickClass');  
+})
+
+$(function() {
+	$('#person-message').trigger('click')
+})
--- js/youke-front.js
+++
@@ -1,9 +0,0 @@
-$('#up').click(function () {
-	var number = $(this).find('span').innerHTML;
-	if(number==undefined) {
-		$(this).find('span').innerHTML=1;
-	}
-	else {
-		number++;
-	}
-})
